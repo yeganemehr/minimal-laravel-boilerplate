b72f39fd52db88aeeae6a0572f7feb7418ff6f58
Amir H. Yeganemehr <yeganemehr@yahoo.com>
2023-09-10 14:00:34 +0330

    Apply my changes

---
+++ src/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\ServiceProvider as SupportServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class ServiceProvider extends SupportServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $this->app->booted(function () {
+            $this->app['router']->getRoutes()->refreshNameLookups();
+            $this->app['router']->getRoutes()->refreshActionLookups();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        Route::middleware('api')
+            ->prefix("api")
+            ->name('api.')
+            ->group(__DIR__ . '/../routes/api.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    }
+}
